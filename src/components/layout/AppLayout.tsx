679c1acd1f15eea478b1f415badabdf8bff79197
Emir ÖzArslan <emirhandesdemir@gmail.com>
2025-06-14 09:41:07 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as React from 'react'; // Added React import
 import type { ReactNode } from 'react';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
