cc6a2d4bf4d4d03afd865f496b9e936b9171e786
Emir ÖzArslan <emirhandesdemir@gmail.com>
2025-06-20 06:54:30 +0000

    9:52:00 AM: buildbot version: c7d789db33033d118ad9bb27d4968225e0f4a584 9

--- src/components/admin/sections/AdminGameSettingsContent.tsx
+++ src/components/admin/sections/AdminGameSettingsContent.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import React, { useEffect, useState, FormEvent, useCallback } from "react"; // React importu eklendi
+import React, { useEffect, useState, FormEvent, useCallback } from "react";
 import { db } from "@/lib/firebase";
 import { doc, getDoc, updateDoc, setDoc, collection, addDoc, deleteDoc as deleteFirestoreDoc, query, orderBy, onSnapshot, serverTimestamp, type Timestamp, getDocs } from "firebase/firestore";
 import { useAuth } from "@/contexts/AuthContext";
@@ -402,5 +402,3 @@ export default function AdminGameSettingsContent() {
     </div>
   );
 }
-
-    
