8fe5b159e6c30ccc0798c7a980e728bc247745ee
Emir ÖzArslan <emirhandesdemir@gmail.com>
2025-06-21 18:12:28 +0000

    Admin panelinde tasarimi yeniden tasarla ust bar tam oturmamış ayrica ku

---
+++ opt/build/repo/src/components/admin/AdminOverlayPanel.tsx
@@ -0,0 +1,13 @@
+
+"use client";
+
+import React from 'react';
+
+// This component is deprecated and no longer in use.
+// The admin panel functionality has been moved to a dedicated page layout
+// under src/app/(admin)/layout.tsx for a better user experience.
+// This file can be safely removed from the project in the future.
+
+export default function AdminOverlayPanel() {
+  return null;
+}
