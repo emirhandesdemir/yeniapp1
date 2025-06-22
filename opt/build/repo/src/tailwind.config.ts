9df092fe4ca126e398c996e3802220ee7c92d161
Emir ÖzArslan <emirhandesdemir@gmail.com>
2025-06-22 04:22:56 +0000

    7:21:21 AM: build-image version: b4a14dfbcd8b107dda361870fa2c4c1e6a145af

---
+++ opt/build/repo/src/tailwind.config.ts
@@ -0,0 +1,5 @@
+
+// This file is deprecated. The main configuration is in the root `tailwind.config.js`.
+// This file is modified to re-export the root config to prevent build errors.
+const config = require('../../tailwind.config.js');
+module.exports = config;
---
+++ opt/build/repo/tailwind.config.ts
@@ -0,0 +1,3 @@
+// This file is deprecated. The main configuration is in the root `tailwind.config.js`.
+// This file is modified to prevent build errors.
+module.exports = {};
